22f587e9815a063d201e1430ae7833b7d8292097
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-18 02:07:09 +0800

    調整store to upload
    補上@method(PATCH)

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -6,20 +6,16 @@
 
 @section('main')
     <div class="container">
-        {{-- <form action="{{route('news.store')}}" method="POST"> --}}
-        {{-- 用下面這條會報錯419，但是我要傳圖片啊... --}}
+
         <div class="row">
             <div class="col-8 mx-auto">
-                <form action="{{ route('news.store') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{route('news.update',['id' =>$news->id])}}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    @method('PATCH')
                     <div class="row">
                         <label for="title" class="form-label">文章標題</label>
                         <input type="text" id="title" name="title" class="form-control" value="{{$news->title}}">
                     </div>
-                    {{-- <div class="row">
-					<label for="author">文章作者</label>
-					<input type="text" id="author" name="author">
-				</div> --}}
                     <div class="row custom">
                         <label for="content" class="form-label">文章內容</label>
                         <textarea name="content" id="content" class="form-control">{{$news->content}}</textarea>
