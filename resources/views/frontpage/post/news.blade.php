c229c11e78ff3e716748e2b75af77a3f38b26aae
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-18 10:32:13 +0800

    news引入資料
    不過圖片需要設置好寬高統一
    然後文字css要加overflow

--- resources/views/frontpage/post/news.blade.php
+++ resources/views/frontpage/post/news.blade.php
@@ -48,32 +48,23 @@
         </section>
         <section class="news_content">
             <div class="container">
+
+                @foreach ($news as $item)    
+
                 <div class="row">
                     <div class="col-4">
-                        <img class="w-100" src="./team-img/news_pic/1.png" alt="">
+                        <img class="w-100" src="{{$item->image}}" alt="">
                     </div>
                     <div class="col-8">
                         <h5 class="bg-gradient text-white bg-opacity-75 d-inline-flex p-2 bd-highlight text-center mb-4">最新消息</h5>
-                        <h3 class="outline mb-4"><a href="#" target="_self">貓尾巴寵物美容 一週快閃限定活動</a></h3>
-                        <h6 class="text-success mb-3">2022.12.29</h6>
-                        <p>我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 ....</p>
+                        <h3 class="outline mb-4"><a href="#" target="_self">{{$item->title}}</a></h3>
+                        <h6 class="text-success mb-3">{{$item->created_at}}</h6>
+                        <p>{{$item->content}}</p>
                     </div>
                 </div>
                 <hr class="my-5">
-                <div class="row">
-                    <div class="col-4">
-                        <img class="w-100" src="./team-img/news_pic/2.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h5
-                            class="bg-gradient text-white bg-opacity-75 d-inline-flex p-2 bd-highlight text-center mb-4">
-                            最新消息</h5>
-                        <h3 class="outline mb-4"><a href="#" target="_self">總柴抱抱寵物攝影 新年優惠</a></h3>
-                        <h4 class="text-danger mb-3 fs-3">2022.12.29</h4>
-                        <p>我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 ....</p>
-                    </div>
-                </div>
-                <hr class="my-5">
+                @endforeach
+
                 <div class="row">
                     <div class="col-4">
                         <img class="w-100" src="./team-img/news_pic/3.png" alt="">
@@ -88,20 +79,7 @@
                     </div>
                 </div>
                 <hr class="my-5">
-                <div class="row">
-                    <div class="col-4">
-                        <img class="w-100" src="./team-img/news_pic/4.png" alt="">
-                    </div>
-                    <div class="col-8">
-                        <h5
-                            class="bg-gradient text-white bg-opacity-75 d-inline-flex p-2 bd-highlight text-center mb-4">
-                            最新消息</h5>
-                        <h3 class="outline mb-4"><a href="#" target="_self">[毛寶貝] 到府服務</a></h3>
-                        <h4 class="text-danger mb-3 fs-3">2022-01-07</h4>
-                        <p>我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 我是假文 ....</p>
-                    </div>
-                </div>
-                <hr class="my-5">
+
             </div>
 
             <!-- 按鈕區塊 -->
