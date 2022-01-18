177daba4574767aeaedb00f086d5cebf29efad6f
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-19 01:24:38 +0800

    微調modal

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -45,7 +45,7 @@
                 <td>{{$item->created_at}}</td>
 				{{-- <td>{{($item->role)}}</td> --}}
                 <td>
-					<button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal">
+					{{-- <button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal">
 						重設密碼
 					  </button>					  
 					  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -62,15 +62,43 @@
 								<input type="password" name="password" id="password">
 							</div>
 							<div class="modal-footer">
-								{{-- <form action="/user/update/{{$user->id}}" method="post"> --}}
+								<form action="/user/update/{{$user->id}}" method="post">
 									@csrf
 									<button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
 									<button type="submit" class="btn btn-danger" data-dismiss="modal">確定</button>
-								{{-- </form> --}}
+								</form>
 							</div>
 						  </div>
 						</div>
-					  </div>
+					  </div> --}}
+
+					<!-- Button trigger modal -->
+					<button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+						重設密碼
+					</button>
+					
+					<!-- Modal -->
+					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">密碼重設一次收費500元</h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body">
+								<div class="modal-body">
+									<label for="password">幫助使用者重設密碼</label>
+									<input type="password" name="password" id="password">
+								</div>
+							</div>
+							<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
+							<button type="button" class="btn btn-primary">確定</button>
+							</div>
+						</div>
+						</div>
+					</div>
+  {{-- 下面不是 --}}
 					<button type="button" class="btn btn-outline-info disabled" aria-disabled="true">凍結</button>
 					<button type="button" class="btn btn-outline-danger">刪除</button>
                     <a href="/user/info/{{$item->id}}">老師那顆</a>
