1ca4d277cdf1f9e857a2ea8651a2bc4fdef6b558
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-19 10:14:22 +0800

    先預加調整type的程式碼並註解 等問題解決後解封

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -16,6 +16,14 @@
                         <label for="title" class="form-label">文章標題</label>
                         <input type="text" id="title" name="title" class="form-control" value="{{$news->title}}">
                     </div>
+                    {{-- <select class="form-select" aria-label="Default select example" name="type">
+                        <option selected>文章類型</option>
+                        <option value="{{$news->type}}">{{$news->type}}</option>
+                        <option value="最新消息">最新消息</option>
+                        <option value="寵物保姆">寵物保姆</option>
+                        <option value="動保法律">動保法律</option>
+                        <option value="一起回家故事">一起回家故事</option>
+                    </select> --}}
                     <div class="row custom">
                         <label for="content" class="form-label">文章內容</label>
                         <textarea rows="20" name="content" id="content" class="form-control">{{$news->content}}</textarea>
