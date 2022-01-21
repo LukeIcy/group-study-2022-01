4113b696f0b712d98df0be29b4aaa2c034d1192f
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-21 23:57:14 +0800

    串聯網頁

--- app/Http/Controllers/AdopController.php
+++ app/Http/Controllers/AdopController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AdopController extends Controller
 {
-    //
+    // 寵物照片(前台)
+    public function index()
+    {
+        return view('frontpage.center.adoption');
+    }
+
+    // 動物資訊&申請表(前台)
+    public function apply()
+    {
+        return view('frontpage.center.animal');
+    }
 }
