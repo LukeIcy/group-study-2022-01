d84d8b6dc2ac823ae474e4ce128da2b438414f47
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-23 03:08:50 +0800

    user model補上fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
+        'avatar',
+        'career',
+        'adopnumber',
+        'experience',
     ];
 
     /**
