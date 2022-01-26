aed2078e4ff3a75437280c0737bc8ed4ca085518
LukeIcy <95893128+LukeIcy@users.noreply.github.com>
2022-01-26 14:28:57 +0800

    建立後台中介層BackStage

---
+++ app/Http/Middleware/BackStage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BackStage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()){
+            // 只寫x OR y 跟 x || y 都失效 是要下面這樣
+            if (Auth::user()->role == '管理者' || Auth::user()->role == '工程師'){
+                return $next($request);
+            }
+            return redirect('/product');
+        }
+    }
+}
